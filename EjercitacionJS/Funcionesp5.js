ee50eb66c9d1d854e6fc8e953882daef1c992b1e
Ximena Carmona <ximecaarmona@gmail.com>
2024-11-09 23:58:52 -0300

    TP4Actualizado

--- EjercitacionJS/Funcionesp5.js
+++
@@ -1,4 +0,0 @@
-//Crea una función llamada actualizarEdad que tome un objeto
-//persona y un número nuevaEdad, y actualice la propiedad edad del objeto.
-
-//function actualizarEdad()
--- EjercitacionJS/Objetospunto1.js
+++
@@ -1,9 +0,0 @@
-//Crea un objeto llamado libro con propiedades como título, autor y añoDePublicacion. Imprime cada propiedad en la consola.
-
-const libro = {
-    titulo:'titulo',
-    autor: 'autor',
-    anioPublicacion: 'añopublic'
-}
-
-console.log(libro.anioPublicacion, libro.autor, libro.titulo)
